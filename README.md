728d6656a87c834b9b07455ac25c00ccbdb6cd17
fouad.yamani <fyamani@acefy.fr>
2014-04-20 13:36:16 +0000

    clean up

--- README.md
+++ README.md
@@ -2,3 +2,10 @@ sandbox
 =======
 
 Sandbox for Business APP's
+
+
+Installation
+============
+
+
+php composer.phar create-project bf13/sandbox path/ dev-master
---
+++ bin/update.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+set -e
+
+if [ -z "$1" ]; then
+  env="dev"
+else
+  env=$1
+fi
+
+echo "##########################################################"
+
+echo "Environment: " $env
+echo ""
+
+echo ">>> Cleanup cache and vendor"
+rm -rf app/cache/*
+rm -rf vendor
+
+if [ ! -f composer.phar ]; then
+    echo ">>> Downloading composer.phar"
+    curl -s http://getcomposer.org/installer | php
+fi
+
+if [ ! -d vendor ]; then
+    echo ">>> Installing dependencies"
+    php composer.phar update
+fi
+
+echo ">>> Installing assets"
+rm -Rf web/bundles
+php app/console assets:install
+
+echo ">>> Update Database"
+php app/console doctrine:schema:update --force
+
+echo ">>> Dumping assetic"
+# rm -Rf web/{css,js}/*
+php app/console assetic:dump web
+php app/console assetic:dump web --env=prod
